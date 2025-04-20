950a230d41ce29284ba4201f01854a71e9ca7282
Gyoza-Gyoza <136580712+Gyoza-Gyoza@users.noreply.github.com>
2025-04-21 00:17:22 +0800

    completed keybinding system

---
+++ Assets/Entities/Player/Player Controller.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public void Movement(Vector2 amount)
+    {
+
+    }
+    public void Jump(float amount)
+    {
+
+    }
+}
---
+++ Assets/Systems/KeyBindings/KeyInputSelector.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class KeyInputSelector : MonoBehaviour
+{
+    public KeyInput keyInput;
+    private KeyCode keyCode;
+    public KeyCode KeyCode
+    {  
+        get { return keyCode; } 
+        set 
+        { 
+            keyCode = value;
+            keyText.text = value.ToString();
+        }
+    }
+    [HideInInspector]
+    public TextMeshProUGUI keyText;
+
+    private bool setKeyBindMode;
+    private void Awake()
+    {
+        keyText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+    private void Start()
+    {
+        InitializeSelector();
+    }
+    private void Update()
+    {
+        if (setKeyBindMode)
+        {
+            foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+            {
+                //Gets the current pressed key 
+                if (Input.GetKey(kcode))
+                {
+                    if(IsKeycodeAvailable(kcode))
+                    {
+                        KeyCode = kcode;
+                        setKeyBindMode = false;
+                        break;
+                    }
+                    else
+                    {
+                        KeyCode = keyCode;
+                        setKeyBindMode = false;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    private void InitializeSelector()
+    {
+        KeyBindManager.Instance.GetSelectorReference(this);
+        KeyCode = KeyBindManager.Instance.Keys[keyInput];
+    }
+    /// <summary>
+    /// Start key binding mode, for use with Unity Buttons
+    /// </summary>
+    public void StartKeyBindMode()
+    {
+        setKeyBindMode = true;
+        keyText.text = "Enter key";
+    }
+
+    /// <summary>
+    /// Checks to see if the keycode is not being used in the keybind list
+    /// </summary>
+    /// <param name="keycodeToCheck">The keycode that is being checked</param>
+    /// <returns></returns>
+    private bool IsKeycodeAvailable(KeyCode keycodeToCheck)
+    {
+        foreach(var kvp in KeyBindManager.Instance.Keys)
+        {
+            if (keycodeToCheck == kvp.Value)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
