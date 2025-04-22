c6e8f32c4a9f2c4f7218994a3243ea7449cf4e5e
Gyoza-Gyoza <calebwjle@gmail.com>
2025-04-22 11:29:42 +0800

    Completed notification

--- Assets/Systems/Screen Effects System/ScreenEffectsManager.cs
+++ Assets/Systems/Screen Effects System/ScreenEffectsManager.cs
@@ -11,34 +11,62 @@ public class ScreenEffectsManager : MonoBehaviour
     [SerializeField]
     private Image screenCover;
     [SerializeField]
-    private TextMeshProUGUI message;
+    private TextMeshProUGUI notification;
     private void Awake()
     {
         if (Instance == null) Instance = this;
     }
+    private void Start()
+    {
+        screenCover.gameObject.SetActive(false);
+        notification.gameObject.SetActive(false);
+    }
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.P)) ScreenFade();
+        if (Input.GetKeyUp(KeyCode.O)) CreateTextNotification("Test");
     }
+
+    /// <summary>
+    /// Performs a full screen fade in and out
+    /// </summary>
+    /// <param name="fadeDuration">Duration taken to fade in/out</param>
+    /// <param name="holdDuration">Duration that the fade will hold for</param>
+    /// <param name="color">Color of the screen fade, leave as null for black</param>
     public void ScreenFade(float fadeDuration = 0.5f, float holdDuration = 1f, Color? color = null)
     {
         //Assigns chosen values to the screen 
         screenCover.color = color ?? Color.black;
         
-        StartCoroutine(ScreenFadeCoroutine(fadeDuration, holdDuration));
+        StartCoroutine(FadeCoroutine(screenCover, fadeDuration, holdDuration));
     }
-    private IEnumerator ScreenFadeCoroutine(float fadeDuration, float holdDuration)
+
+    /// <summary>
+    /// Creates a text notification 
+    /// </summary>
+    /// <param name="notificationText">Text to display</param>
+    /// <param name="fadeDuration">Duration taken to fade in/out</param>
+    /// <param name="holdDuration">Duration that the fade will hold for</param>
+    public void CreateTextNotification(string notificationText, float fadeDuration = 0.5f, float holdDuration = 1f)
+    {
+        //Assigns text to the notification
+        notification.text = notificationText;
+
+        StartCoroutine(FadeCoroutine(notification, fadeDuration, holdDuration));
+    }
+
+    private IEnumerator FadeCoroutine(Graphic target, float fadeDuration, float holdDuration)
     {
         //Fade in
-        screenCover.gameObject.SetActive(true);
-        StartCoroutine(Fade(screenCover, fadeDuration, true));
+        target.gameObject.SetActive(true);
+        StartCoroutine(Fade(target, fadeDuration, true));
 
         //Hold
         yield return new WaitForSeconds(holdDuration);
 
         //Fade out
-        yield return StartCoroutine(Fade(screenCover, fadeDuration, false));
-        screenCover.gameObject.SetActive(false);
+        yield return StartCoroutine(Fade(target, fadeDuration, false));
+        target.gameObject.SetActive(false);
     }
 
     /// <summary>
@@ -51,7 +79,7 @@ public class ScreenEffectsManager : MonoBehaviour
     private IEnumerator Fade(Graphic target, float fadeDuration, bool fadeIn)
     {
         float timer = 0f;
-
+        
         float startAlpha = fadeIn ? 0f : 1f;
         float endAlpha = fadeIn ? 1f : 0f;
 
