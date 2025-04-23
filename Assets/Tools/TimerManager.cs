d891acf92d37b075cea0cd645f2287e1bf14626e
Gyoza-Gyoza <136580712+Gyoza-Gyoza@users.noreply.github.com>
2025-04-24 02:43:05 +0800

    updated timer manager

--- Assets/Tools/TimerManager.cs
+++ Assets/Tools/TimerManager.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerManager : MonoBehaviour
 {
     public static TimerManager Instance;
-    private List<TimedAction> timedActions = new List<TimedAction>();
-    private List<TimedRoutine> timedRoutines = new List<TimedRoutine>();
+    private List<TimedTask> timedTasks = new List<TimedTask>();
+    public List<TimedTask> TimedTasks
+    { get { return timedTasks; } }
 
     private void Awake()
     {
@@ -16,51 +19,22 @@ public class TimerManager : MonoBehaviour
     }
     private void Update()
     {
-        foreach (var action in timedActions)
+        for (int i = timedTasks.Count - 1; i >= 0; i--)
         {
-            if (action.isActive)
-            {
-                action.elapsedTime += Time.deltaTime;
-                if (action.elapsedTime >= action.duration)
-                {
-                    action.onComplete?.Invoke();
-                    action.isActive = false;
-                }
-            }
-        }
-        foreach(var routine in timedRoutines)
-        {
-            if (routine.isActive)
-            {
-                routine.elapsedTime += Time.deltaTime;
-                if (routine.elapsedTime <= routine.duration) routine.routine?.Invoke();
-                else routine.isActive = false;
-            }
+            if (timedTasks[i].IsActive) timedTasks[i].Tick();
+            else timedTasks.RemoveAt(i);
         }
     }
     #region Object Pooling
     /// <summary>
-    /// Get a timed action from the pool
+    /// Initializes a new timed action
     /// </summary>
     /// <param name="duration">Duration before action will be performed</param>
     /// <param name="onComplete">Action that will be performed after duration has passed</param>
     /// <returns></returns>
     public void CreateTimedAction(float duration, Action onComplete)
     {
-        foreach(var timedAction in timedActions)
-        {
-            //If there is an available object, reuse it
-            if (!timedAction.isActive)
-            {
-                timedAction.isActive = true;
-                timedAction.duration = duration;
-                timedAction.elapsedTime = 0f;
-                timedAction.onComplete = onComplete;
-                return;
-            }
-        }
-        //If there are no available objects, create a new one
-        timedActions.Add(new TimedAction(duration, onComplete));
+        timedTasks.Add(TimedAction.Get(duration, onComplete));
     }
 
     /// <summary>
@@ -71,49 +45,114 @@ public class TimerManager : MonoBehaviour
     /// <returns></returns>
     public void CreateTimedRoutine(float duration, Action routine)
     {
-        foreach (var timedRoutine in timedRoutines)
-        {
-            //If there is an available object, reuse it
-            if (!timedRoutine.isActive)
-            {
-                timedRoutine.isActive = true;
-                timedRoutine.duration = duration;
-                timedRoutine.elapsedTime = 0f;
-                timedRoutine.routine = routine;
-                return;
-            }
-        }
-        //If there are no available objects, create a new one
-        timedRoutines.Add(new TimedRoutine(duration, routine));
+        timedTasks.Add(TimedRoutine.Get(duration, routine));
     }
     #endregion
 }
 
-public class TimedAction
+public abstract class TimedTask
 {
-    public float duration;
-    public float elapsedTime;
-    public bool isActive;
-    public Action onComplete;
-    public TimedAction(float duration, Action onComplete)
+    public float Duration;
+    public float ElapsedTime;
+    public bool IsActive;
+
+    public TimedTask(float duration)
     {
-        this.duration = duration;
-        this.onComplete = onComplete;
-        isActive = true;
-        elapsedTime = 0f;
+        Duration = duration;
+        ElapsedTime = 0f;
+        IsActive = true;
+    }
+    public abstract void Tick();
+}
+public class TimedAction : TimedTask
+{
+    private static Stack<TimedAction> timedActions = new Stack<TimedAction>();
+
+    public Action OnComplete;
+    private TimedAction(float duration, Action onComplete) : base(duration)
+    {
+        OnComplete = onComplete;
+    }
+
+    /// <summary>
+    /// Returns a TimedAction
+    /// </summary>
+    /// <param name="duration">Duration before action will be performed</param>
+    /// <param name="onComplete">Action that will be performed after duration has passed</param>
+    /// <returns></returns>
+    public static TimedAction Get(float duration, Action onComplete)
+    {
+        if (timedActions.Count > 0) //Checks if there are objects in the pool 
+        {
+            TimedAction result = timedActions.Pop();
+
+            result.Duration = duration;
+            result.ElapsedTime = 0f;
+            result.IsActive = true;
+            result.OnComplete = onComplete;
+
+            return result;
+        }
+        else return new TimedAction(duration, onComplete);
+    }
+    public void Return()
+    {
+        timedActions.Push(this);
+    }
+    public override void Tick()
+    {
+        ElapsedTime += Time.deltaTime;
+        if (ElapsedTime >= Duration)
+        {
+            OnComplete?.Invoke();
+            IsActive = false;
+            Return();
+        }
     }
 }
-public class TimedRoutine
+public class TimedRoutine : TimedTask
 {
-    public float duration;
-    public float elapsedTime;
-    public bool isActive;
-    public Action routine;
-    public TimedRoutine(float duration, Action routine)
+    private static Stack<TimedRoutine> timedRoutines = new Stack<TimedRoutine>();
+
+    public Action Routine;
+    private TimedRoutine(float duration, Action routine) : base(duration) 
     {
-        this.duration = duration;
-        this.routine = routine;
-        isActive = true;
-        elapsedTime = 0f;
+        Routine = routine;
+    }
+
+    /// <summary>
+    /// Returns a TimedRoutine 
+    /// </summary>
+    /// <param name="duration">Duration of the routine being performed</param>
+    /// <param name="onComplete">Action that will be performed over the duration of the routine</param>
+    /// <returns></returns>
+    public static TimedRoutine Get(float duration, Action routine)
+    {
+        if (timedRoutines.Count > 0) //Checks if there are objects in the pool 
+        {
+            TimedRoutine result = timedRoutines.Pop();
+
+            result.Duration = duration;
+            result.ElapsedTime = 0f;
+            result.IsActive = true;
+            result.Routine = routine;
+
+            return result;
+        }
+        else return new TimedRoutine(duration, routine);
+    }
+    public void Return()
+    {
+        timedRoutines.Push(this);
+    }
+    public override void Tick()
+    {
+        ElapsedTime += Time.deltaTime;
+        if (ElapsedTime <= Duration) Routine?.Invoke();
+        else
+        {
+            IsActive = false;
+            Return();
+        }
     }
 }
